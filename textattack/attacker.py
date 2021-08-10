e72409480c0f5beee1a6ba50b182b113eeabf501
Andrew Copenhaver <andrew.copenhaver@maxar.com>
2021-08-10 16:55:58 -0400

    Update attacker.py

--- textattack/attacker.py
+++ textattack/attacker.py
@@ -509,7 +509,7 @@ class Attacker:
 def pytorch_multiprocessing_workaround():
     # This is a fix for a known bug
     try:
-        cloudpickle_multiprocess.set_start_method("spawn", force=True)
+        cmp.set_start_method("spawn", force=True)
         cmp.set_sharing_strategy("file_system")
     except RuntimeError:
         pass
